47345182035188712dca0c801a3095175c20aebd
Adhityaekp <114294208+Adhityaekp@users.noreply.github.com>
2024-11-20 15:48:42 +0700

    Merapikan Code

--- resources/views/user/checkcam.blade.php
+++ resources/views/user/checkcam.blade.php
@@ -1,28 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Document</title>
     <style>
         .btn-primary {
-      background-color: #7C0000;
-      border: none;
-      color: #fafafa;
-      margin-top: 20px;
-    }
+            background-color: #7C0000;
+            border: none;
+            color: #fafafa;
+            margin-top: 20px;
+        }
 
-    .btn-primary:hover {
-      background-color: #7c0000b9; 
-      outline: none;
-      border-color: #7C0000;
-      box-shadow: 0 0 4px hsla(0, 100%, 24%, 0.5);
-      color: #fafafa;
-    }
-        </style>
+        .btn-primary:hover {
+            background-color: #7c0000b9;
+            outline: none;
+            border-color: #7C0000;
+            box-shadow: 0 0 4px hsla(0, 100%, 24%, 0.5);
+            color: #fafafa;
+        }
+    </style>
 </head>
+
 <body class="d-flex flex-column justify-content-center align-items-center vh-100 bg-light">
 
     <div class="position-absolute top-0 start-0 m-3">
@@ -32,11 +35,18 @@
     <div class="text-center">
         <h3 class="mb-2">Halo, Budi!</h3>
         <h1 class="mb-4" style="font-size: 48px;">Selamat Datang di Psikotes-ILC</h1>
-       <div style="max-width: 650px;"> 
-        <p class="mb-2" style="text-align: center; font-size: 24px; font-weight: 500; color:rgba(0, 0, 0, 0.5)">Sebelum memulai, mari kita berikan sedikit informasi mengenai psikotest ini.</p>
-        <p class="mb-4" style="color:rgba(0, 0, 0, 0.5)">Psikotest ini dirancang khusus untuk mengukur beberapa aspek kepribadian dan keterampilan kognitif Anda, yang akan menjadi bagian dari syarat kelulusan dalam seleksi Mechanical Class. Hasil dari psikotest ini akan membantu kami memastikan kesiapan Anda untuk mengikuti pelatihan dan memaksimalkan potensi yang dimiliki.</p>
-       </div>   <a href="/mulai" class="btn btn-primary px-5 py-2">Mulai</a>
+        <div style="max-width: 650px;">
+            <p class="mb-2" style="text-align: center; font-size: 24px; font-weight: 500; color:rgba(0, 0, 0, 0.5)">
+                Sebelum memulai, mari kita berikan sedikit informasi mengenai psikotest ini.</p>
+            <p class="mb-4" style="color:rgba(0, 0, 0, 0.5)">Psikotest ini dirancang khusus untuk mengukur beberapa
+                aspek kepribadian dan keterampilan kognitif Anda, yang akan menjadi bagian dari syarat kelulusan dalam
+                seleksi Mechanical Class. Hasil dari psikotest ini akan membantu kami memastikan kesiapan Anda untuk
+                mengikuti pelatihan dan memaksimalkan potensi yang dimiliki.</p>
+        </div> <a href="/mulai" class="btn btn-primary px-5 py-2">Mulai</a>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script>
 </body>
+
 </html>
